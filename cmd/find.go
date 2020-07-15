b314ed6884e22fa14a4931be304092984654756b
Abraham <cedenoabraham@gmail.com>
2020-07-14 23:44:08 -0300

    playing a lil bit

--- cmd/find.go
+++ cmd/find.go
@@ -2,27 +2,45 @@ package cmd
 
 import (
 	"fmt"
-	// "strconv"
-
 	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
 )
 
 // find represents the find command
 var find = &cobra.Command{
 	Use:   "find",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "find the occurence of any word or phrase throughout the entire project",
+	Long: `given a word or phrase find where in the project is located what you're 
+lookig for. You'll get a list of files in which were found matches.
+
+For example:
+
+term find --path /home/user/project `,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		fmt.Println("Running find command :D")
+
+		path, _ := cmd.Flags().GetString("path")
+
+		fmt.Println("path: ", path)
+
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(path, info.Size())
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(find)
-	// addCmd.Flags().BoolP("float", "f", false, "Add Floating Numbers")
+	find.Flags().String("path", "", "--path [directory]")
+	find.Flags().BoolP("case", "c", false, "--case")
 }
